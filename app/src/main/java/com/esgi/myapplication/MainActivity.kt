c39e0806cc8ef679f391194f5238792ff3b0de2c
julien-guillan <guillan.julien@live.com>
2021-01-18 16:24:01 +0100

    Repo creation TeamCity TP

---
+++ app/src/main/java/com/esgi/myapplication/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.esgi.myapplication
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.RecyclerView
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+    var directory: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        var recyclerView = findViewById<RecyclerView>(R.id.filesRecyclerView)
+
+        File("/storage").walk().forEach {
+            println(it)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun getCurrentDir(){
+        val path = System.getProperty("user.dir")
+        println("USER DIR: $path")
+
+
+    }
+}
---
+++ app/src/main/java/com/esgi/myapplication/RecyclerAdapter.java
@@ -0,0 +1,4 @@
+package com.esgi;
+
+class RecyclerAdapter{
+}
